ebce0b92d6645aea10ac3048a5186025d40046fc
Rainer Koirikivi <rainer@koirikivi.fi>
2015-03-02 02:04:29 +0200

    Implement simple kernel module

---
+++ simple.c
@@ -0,0 +1,23 @@
+/*
+ * Example kernel module
+ */
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+int simple_init(void) {
+    printk(KERN_INFO "Loading Module\n");
+    return 0;
+}
+
+int simple_exit(void) {
+    printk(KERN_INFO "Removing Module\n");
+    return 0;
+}
+
+module_init(simple_init);
+module_exit(simple_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
